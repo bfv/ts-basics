c8dbc69c2a54cbc77541424d62d43164d2d22265
bfv <dev@bfv.io>
2018-12-24 16:24:24 +0100

    remove Set from index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,5 @@ import * as test from './test';
 
 export { LinkedList } from './linkedlist';
 export { Queue } from './queue';
-export { Set } from './set';
 export { Stack } from './stack';
 export { Matrix } from './matrix';
